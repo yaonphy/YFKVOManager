af01880b10ba555bab8424d4b29897e1d37bc7d1
yaonphy <yaongfeng@gmail.com>
2017-05-03 18:40:54 +0800

    design key

--- YFKVOManagerCase/YFKVOManagerCase/ViewController.swift
+++ YFKVOManagerCase/YFKVOManagerCase/ViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
+
+class PersonModel: NSObject {
+    dynamic var name = ""
+
+}
+
+
 class ViewController: UIViewController {
+    
+    var kvoContext:String = "kvoContext"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let thePerson: PersonModel = PersonModel.init()
+    
+        thePerson.addObserver(self, forKeyPath: "name", options: NSKeyValueObservingOptions([.new,.old]), context: &kvoContext)
         
         
-        
+        thePerson.name = "haha"
+
+        thePerson.name = "hahahahahahahahahaha"
+
     }
 
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if let theChage = change {
+            print(theChage[NSKeyValueChangeKey.newKey] ?? "null")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
